8cd68c938137fe0b1e45316a62d89c8a2b3bf200
skhshkk <165941835+skhshkk@users.noreply.github.com>
2025-06-07 21:28:47 +0530

    app.py

--- app.py
+++ app.py
@@ -318,7 +318,7 @@ st.markdown("---")
 st.markdown(
     """
     <div style="text-align: center; color: #999; font-size: 0.85rem;">
-      Built with ❤️ using Streamlit · Anime dataset from MyAnimeList.com
+      Built with 🪐 using Streamlit · Anime dataset from MyAnimeList.com
     </div>
     """,
     unsafe_allow_html=True,
